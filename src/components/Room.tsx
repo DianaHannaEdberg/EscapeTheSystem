f54c640b07a3737a9d4127c3b37dd6fbdad385b9
Diana Hanna <dianahannaedberg@gmail.com>
2026-06-15 08:06:33 +0200

    context

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 import rooms from "./../data/rooms.json"; 
-import items from '../data/items.json'
-import { useInventory } from './InventoryContext'
+import items from "../data/items.json";
+import { useInventory } from "./InventoryContext"; 
 
 
 const Room = () => {
     const { roomPath } = useParams();  
-    const { inventory, addItem } = useInventory()
+
+    const { inventory, addItem } = useInventory();
 
    const room = rooms.find(r => r.roomPath === roomPath); 
 
@@ -14,25 +15,42 @@ const Room = () => {
     return <h1>Room not found</h1>
    }
 
-  const roomIsSolved = inventory.some(i => i.id === room.itemToAdd)
+
+  const roomIsSolved = inventory.some( 
+    (i) => i.id === room.itemToAdd);
+
    const handleItemClick = (item) => {
     if (item.id === room.itemToSolve && !roomIsSolved) {
-      const reward = items.find(i => i.id === room.itemToAdd)
-      if (reward) addItem(reward)
+      const reward = items.find(
+        (i)=> i.id === room.itemToAdd);
+
+
+      if (reward)
+        addItem(reward)
     }
   }
-
+        let instruction = room.unsolvedInstruction;
+        
+        if (roomIsSolved) {
+          instruction = room.solvedInstruction;
+        }
 
   return (
     <div>
         <h1>{ room.roomName }</h1>
-            <p>{roomIsSolved ? room.solvedInstruction : room.unsolvedInstruction}</p>
-        
+
+        <p>{instruction}</p>
+
         <div>
-      {inventory.map((inventoryItem) => (
-        <button key={inventoryItem} onClick={() => handleItemClick(inventoryItem)}>
+      
+        {inventory.map((inventoryItem) => (
+
+        <button key={inventoryItem} 
+        onClick={() => handleItemClick(inventoryItem)}>
+
           {inventoryItem.item}
         </button>
+
       ))}
     </div>
         </div>
